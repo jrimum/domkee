2c2ba2446fa4cfca1a3f939abe45468c6e02b494
Rômulo Augusto <romulomail@gmail.com>
2010-01-24 23:00:21 +0000

    TICKET [http://jrimum.nordestefomento.com.br/jrimum/ticket/77 #JRimum 77]:  - Remoção do pacote experimental br.com.nordestefomento.jrimum.domkee.base

--- src/main/java/br/com/nordestefomento/jrimum/domkee/base/CodigoIdentificadorVerificavel.java
+++
@@ -1,8 +0,0 @@
-package br.com.nordestefomento.jrimum.domkee.base;
-
-public class CodigoIdentificadorVerificavel<T,V> extends CodigoIdentificador<T>{
-
-	//TODO verificador em vallia
-	
-	V verificador;
-}
--- src/main/java/br/com/nordestefomento/jrimum/domkee/base/Nome.java
+++
@@ -1,5 +0,0 @@
-package br.com.nordestefomento.jrimum.domkee.base;
-
-public class Nome {
-
-}
